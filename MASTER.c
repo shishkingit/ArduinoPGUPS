dfcff924f7fef9b4f34f656afe2772f169be56cb
mthrkali <shishkin_git@mail.ru>
2024-12-25 21:12:36 +0300

    Update MASTER.c

--- MASTER.c
+++ MASTER.c
@@ -1,28 +1,21 @@
 #include <avr/io.h>
-#include <stdint.h> // Для uint16_t
-
-#define F_CPU 16000000UL // Clock frequency for Arduino NANO
-
+#include <stdint.h>
+#define F_CPU 16000000UL
 #include <util/delay.h>
-// MAX6675 pins
 #define THERMO_CLK 6
 #define THERMO_CS  5
 #define THERMO_SO  4
-// MAX487 REDE pin
 #define MAX487REDE_PIN 2
-// Optocoupler pin
+
 #define OPTO_PIN 7
 
-// Function to read temperature from MAX6675
 float read_temperature() {
     uint16_t value = 0;
     int i;
 
-    // Enable MAX6675
     PORTD &= ~(1 << THERMO_CS);
     _delay_us(1);
-
-    // Read data (16 bits)
+  
     for (i = 0; i < 16; i++) {
         PORTD |= (1 << THERMO_CLK);
         value <<= 1;
@@ -32,29 +25,27 @@ float read_temperature() {
         PORTD &= ~(1 << THERMO_CLK);
     }
 
-    // Disable MAX6675
+    //ВЫКЛ MAX6675
     PORTD |= (1 << THERMO_CS);
 
-    // Convert to Celsius (with offset and coefficient)
+    //пересчёт в градусы
     return (0.09 * (value - 793));
 }
 
 int main(void) {
-     // Setup pins as output
+     //установка пиов в режим вывода
     DDRD |= (1 << MAX487REDE_PIN) | (1 << OPTO_PIN) | (1 << THERMO_CS) | (1 << THERMO_CLK);
 
-    // Basic UART initialization
+    //базовая инициализация
     UBRR0H = 0;
-    UBRR0L = 103;  // 9600 baud
-    UCSR0B = (1 << TXEN0);  // Enable transmitter
-
+    UBRR0L = 103;  // 9600
+    UCSR0B = (1 << TXEN0);  //добавление трансмиттера
 
     float t;
     while (1) {
-       // Read temperature
         t = read_temperature();
 
-      // Send temperature via UART
+      //отправление температуры через MAX487
         PORTD &= ~(1 << MAX487REDE_PIN);
        _delay_us(1);
         int i;
@@ -65,7 +56,7 @@ int main(void) {
         PORTD |= (1 << MAX487REDE_PIN);
 
 
-        // Control the optocoupler based on temperature
+        //включение оптопары/лампочки
         if (t > 20) {
             PORTD |= (1 << OPTO_PIN);
             _delay_ms(500);
