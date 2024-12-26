629eba56115d20466af173bd73d3e6e116ce3599
mthrkali <shishkin_git@mail.ru>
2024-12-26 03:03:04 +0300

    Update MASTER.c

--- MASTER.c
+++ MASTER.c
@@ -1,14 +1,21 @@
 #include <avr/io.h>
 #include <stdint.h>
-#define F_CPU 16000000UL
+#define F_CPU 16000000UL //частота ATMEGA328P
 #include <util/delay.h>
+
+//пины MAX6675
 #define THERMO_CLK 6
 #define THERMO_CS  5
 #define THERMO_SO  4
+
+//пины управления MAX487
 #define MAX487REDE_PIN 2
 
+//пин оптопары / лампочки
 #define OPTO_PIN 7
 
+
+//функция чтения температуры с термопары
 float read_temperature() {
     uint16_t value = 0;
     int i;
@@ -25,13 +32,15 @@ float read_temperature() {
         PORTD &= ~(1 << THERMO_CLK);
     }
 
-    //ВЫКЛ MAX6675
+    //выкл MAX6675
     PORTD |= (1 << THERMO_CS);
 
     //пересчёт в градусы
+    //!!!ЦИФРЫ НЕТОЧНЫЕ И ПОЛУЧЕНЫ ОПЫТНЫМ ПУТЁМ!!!
     return (0.09 * (value - 793));
 }
 
+//основная функция с циклом
 int main(void) {
      //установка пиов в режим вывода
     DDRD |= (1 << MAX487REDE_PIN) | (1 << OPTO_PIN) | (1 << THERMO_CS) | (1 << THERMO_CLK);
@@ -54,7 +63,6 @@ int main(void) {
             UDR0 = ((char*)&t)[i];
         }
         PORTD |= (1 << MAX487REDE_PIN);
-
 
         //включение оптопары/лампочки
         if (t > 20) {
